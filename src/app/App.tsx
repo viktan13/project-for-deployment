c964b66bb1189e936306352117a5b30a0b09843b
Viktor Dzyuba <vik.tan@hotmaile.com>
2023-02-05 20:23:43 -0600

    added routeConfig routes refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import {classNames} from "shared/lib/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AboutPage} from "pages/AboutPage";
 import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 
 const App = () => {
@@ -16,12 +17,7 @@ const App = () => {
             <button onClick={toggleTheme}>Toggle Theme</button>
             <Link to={'/'}>Main page</Link>
             <Link to={'/about'}>About page</Link>
-            <Suspense fallback={<div>Loading... </div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };
