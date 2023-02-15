11fbdd10bdd4a4fd124db61ee736d938299bc4d7
Viktor Dzyuba <vik.tan@hotmaile.com>
2023-02-15 12:42:27 -0600

    fixed webpack config file

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
-import { buildCSSLoader } from './loaders/buildCSSLoader';
+import { buildCssLoader } from './loaders/buildCSSLoader';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
@@ -31,7 +31,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         },
     };
 
-    const cssLoader = buildCSSLoader(isDev);
+    const cssLoader = buildCssLoader(isDev);
 
     // Если не используем тайпскрипт - нужен babel-loader
     const typescriptLoader = {
