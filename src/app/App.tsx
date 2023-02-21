c70ad3c6bb1ddcd42fc739d091240f36c015c3e4
Viktor Dzyuba <vik.tan@hotmaile.com>
2023-02-21 17:48:19 -0600

    final global theme fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,15 @@
-import React, { Suspense, useState } from 'react';
+import React, { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Modal } from 'shared/ui/Modal/Modal';
 
 const App = () => {
     const { theme } = useTheme();
 
     return (
-        <div className={classNames('app', {}, [theme])}>
+        <div className={classNames('app', {}, [])}>
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
