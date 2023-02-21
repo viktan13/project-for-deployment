c4f95eb3643909b29a0d8a598be18d434f0bc380
Viktor Dzyuba <vik.tan@hotmaile.com>
2023-02-20 20:09:40 -0600

    added redux store, added first entity

---
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -0,0 +1,18 @@
+import { counterReducer, CounterSchema } from 'entities/Counter';
+import { counterActions } from 'entities/Counter/model/slice/counterSlice';
+
+describe('counterSlice.test', () => {
+    test('increment', () => {
+        const state: CounterSchema = { value: 10 };
+        expect(counterReducer(state, counterActions.increment)).toEqual({ value: 11 });
+    });
+
+    test('decrement', () => {
+        const state: CounterSchema = { value: 10 };
+        expect(counterReducer(state, counterActions.decrement)).toEqual({ value: 9 });
+    });
+
+    test('should work with empty state', () => {
+        expect(counterReducer(undefined, counterActions.increment)).toEqual({ value: 1 });
+    });
+});
--- src/shared/lib/tests/componentsRender/componentRender.tsx
+++ src/shared/lib/tests/componentsRender/componentRender.tsx
@@ -3,22 +3,27 @@ import i18nForTests from 'shared/config/i18n/i18nForTests';
 import { ReactNode } from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { DeepPartial } from '@reduxjs/toolkit';
 
 export interface componentRenderOptions {
     route?: string;
+    initialState?: DeepPartial<StateSchema>
 }
 export const componentRender = (component: ReactNode, options: componentRenderOptions = {}) => {
     const {
         route = '/',
+        initialState,
     } = options;
 
     render(
-
-        <MemoryRouter initialEntries={[route]}>
-            <I18nextProvider i18n={i18nForTests}>
-                {component}
-            </I18nextProvider>
+        <StoreProvider initialState={initialState}>
+            <MemoryRouter initialEntries={[route]}>
+                <I18nextProvider i18n={i18nForTests}>
+                    {component}
+                </I18nextProvider>
+            </MemoryRouter>
             ,
-        </MemoryRouter>,
+        </StoreProvider>,
     );
 };
