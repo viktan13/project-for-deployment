3aadcb4b99ee423f86af3859136427261232876e
Viktor Dzyuba <vik.tan@hotmaile.com>
2023-02-22 23:03:39 -0600

    added new input, added login, form ui

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,18 +1,14 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
-import cls from './LoginModal.module.scss';
 import { LoginForm } from '../LoginForm/LoginForm';
 
 export interface LoginModalProps {
-    className?: string;
     isOpen: boolean;
     onClose: () => void;
 }
 
-export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
+export const LoginModal = ({ isOpen, onClose }: LoginModalProps) => (
     <Modal
         lazy
-        className={classNames(cls.LoginModal, {}, [className])}
         isOpen={isOpen}
         onClose={onClose}
     >
