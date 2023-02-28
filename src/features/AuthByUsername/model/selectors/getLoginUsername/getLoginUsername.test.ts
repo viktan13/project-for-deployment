501827cda45c502bd48541e4b0fa292808fe2d68
Viktor Dzyuba <vik.tan@hotmaile.com>
2023-02-28 15:52:57 -0600

    added tests for login module

---
+++ src/features/AuthByUsername/model/selectors/getLoginUsername/getLoginUsername.test.ts
@@ -0,0 +1,20 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginUsername } from 'features/AuthByUsername/model/selectors/getLoginUsername/getLoginUsername';
+
+describe('getLoginUsername.test', () => {
+    test('should return value', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                username: 'adfinlake',
+            },
+        };
+        expect(getLoginUsername(state as StateSchema)).toEqual('adfinlake');
+    });
+
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {
+        };
+        expect(getLoginUsername(state as StateSchema)).toEqual('');
+    });
+});
