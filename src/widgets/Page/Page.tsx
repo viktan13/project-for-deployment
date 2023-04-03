e4e5fecf2b595c85da34e193df543b14c29e5e16
Viktor Dzyuba <vik.tan@hotmaile.com>
2023-04-03 18:26:23 -0500

    added react virtualized

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -18,6 +18,8 @@ export interface PageProps {
     onScrollEnd?: () => void;
 }
 
+export const PAGE_ID = 'PAGE_ID';
+
 export const Page = memo((props: PageProps) => {
     const { className, children, onScrollEnd } = props;
     const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
@@ -45,6 +47,7 @@ export const Page = memo((props: PageProps) => {
             ref={wrapperRef}
             className={classNames(cls.Page, {}, [className])}
             onScroll={onScroll}
+            id={PAGE_ID}
         >
             {children}
             {onScrollEnd ? <div className={cls.trigger} ref={triggerRef} /> : null}
