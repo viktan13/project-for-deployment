65571d8ea9c511ae524fb10669f4516312826adc
Viktor Dzyuba <vik.tan@hotmaile.com>
2023-04-18 22:50:49 -0500

    some fixes

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -33,10 +33,6 @@ export const ArticleList = memo((props: ArticleListProps) => {
     } = props;
     const { t } = useTranslation();
 
-    const isBig = view === ArticleView.BIG;
-    const itemsPerRow = isBig ? 1 : 5;
-    const rowCount = isBig ? articles.length : Math.ceil(articles.length / itemsPerRow);
-
     if (!isLoading && !articles.length) {
         return (
             <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
