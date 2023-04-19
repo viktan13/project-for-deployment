3df5a7efc60515a984e633baeb0f539d19517905
Viktor Dzyuba <vik.tan@hotmaile.com>
2023-04-18 22:54:24 -0500

    fixed aliases in storybook and jest

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -36,5 +36,7 @@ export default ({ config }: {config: webpack.Configuration}) => {
         __PROJECT__: JSON.stringify('storybook'),
     }));
 
+    config!.resolve!.alias = { '@': path.resolve(__dirname, '..', '..', 'src') };
+
     return config;
 };
