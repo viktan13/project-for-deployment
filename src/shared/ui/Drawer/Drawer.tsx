c32aec43f8cc8f2d261ccde0b6634615b4419515
Viktor Dzyuba <vik.tan@hotmaile.com>
2023-04-18 19:23:13 -0500

    drawer on Overlay close fix

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -83,7 +83,7 @@ export const DrawerContent = memo((props: DrawerProps) => {
     return (
         <Portal>
             <div className={classNames(cls.Drawer, {}, [className, theme, 'app_drawer'])}>
-                <Overlay onClick={close} />
+                <Overlay onClick={() => close()} />
                 <Spring.a.div
                     className={cls.sheet}
                     style={{ display, bottom: `calc(-100vh + ${height - 100}px)`, y }}
