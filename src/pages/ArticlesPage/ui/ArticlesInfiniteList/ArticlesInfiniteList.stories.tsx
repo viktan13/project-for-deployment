27ca90056aa72eccf881b48f508bdfd52c74b9e5
Viktor Dzyuba <vik.tan@hotmaile.com>
2023-05-02 10:56:40 -0500

    added report to CI

--- src/pages/ArticlesPage/ui/ArticlesInfiniteList/ArticlesInfiniteList.stories.tsx
+++ src/pages/ArticlesPage/ui/ArticlesInfiniteList/ArticlesInfiniteList.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 import { ArticlesInfiniteList } from './ArticlesInfiniteList';
-import { ArticleSortField, ArticleType, ArticleView } from '@/entities/Article';
 
 export default {
     title: 'pages/ArticlesPage/ArticlesInfiniteList',
@@ -17,102 +16,4 @@ const Template: ComponentStory<typeof ArticlesInfiniteList> = (args) => <Article
 
 export const Normal = Template.bind({});
 Normal.args = {};
-Normal.decorators = [StoreDecorator({
-    articlesPage: {
-        isLoading: false,
-        error: '',
-        page: 1,
-        limit: 5,
-        hasMore: false,
-        view: ArticleView.SMALL,
-        order: 'desc',
-        sort: ArticleSortField.CREATED,
-        search: '',
-        type: ArticleType.IT,
-        _inited: true,
-        ids: ['1', '2', '3', '4', '5'],
-        entities: {
-            1: {
-                id: '1',
-                title: 'Kotlin news 2019',
-                subtitle: 'Что нового в JS за 2022 год?',
-                img: 'https://miro.medium.com/max/1200/1*FNakkrty3kjOvNU8m5iQfw.png',
-                views: 94002,
-                createdAt: '26.02.2019',
-                user: {
-                    id: '1',
-                    username: 'Ulbi tv',
-                    avatar: 'https://xakep.ru/wp-content/uploads/2018/05/171485/KuroiSH-hacker.jpg',
-                },
-                type: [
-                    ArticleType.IT,
-                ],
-            },
-            2: {
-                id: '2',
-                title: 'Kotlin news 2019',
-                subtitle: 'Что нового в JS за 2022 год?',
-                img: 'https://miro.medium.com/max/1200/1*FNakkrty3kjOvNU8m5iQfw.png',
-                views: 94002,
-                createdAt: '26.02.2019',
-                user: {
-                    id: '1',
-                    username: 'Ulbi tv',
-                    avatar: 'https://xakep.ru/wp-content/uploads/2018/05/171485/KuroiSH-hacker.jpg',
-                },
-                type: [
-                    ArticleType.IT,
-                ],
-            },
-            3: {
-                id: '3',
-                title: 'Kotlin news 2019',
-                subtitle: 'Что нового в JS за 2022 год?',
-                img: 'https://miro.medium.com/max/1200/1*FNakkrty3kjOvNU8m5iQfw.png',
-                views: 94002,
-                createdAt: '26.02.2019',
-                user: {
-                    id: '1',
-                    username: 'Ulbi tv',
-                    avatar: 'https://xakep.ru/wp-content/uploads/2018/05/171485/KuroiSH-hacker.jpg',
-                },
-                type: [
-                    ArticleType.IT,
-                ],
-            },
-            4: {
-                id: '4',
-                title: 'Kotlin news 2019',
-                subtitle: 'Что нового в JS за 2022 год?',
-                img: 'https://miro.medium.com/max/1200/1*FNakkrty3kjOvNU8m5iQfw.png',
-                views: 94002,
-                createdAt: '26.02.2019',
-                user: {
-                    id: '1',
-                    username: 'Ulbi tv',
-                    avatar: 'https://xakep.ru/wp-content/uploads/2018/05/171485/KuroiSH-hacker.jpg',
-                },
-                type: [
-                    ArticleType.IT,
-                ],
-            },
-            5: {
-                id: '5',
-                title: 'Kotlin news 2019',
-                subtitle: 'Что нового в JS за 2022 год?',
-                img: 'https://miro.medium.com/max/1200/1*FNakkrty3kjOvNU8m5iQfw.png',
-                views: 94002,
-                createdAt: '26.02.2019',
-                user: {
-                    id: '1',
-                    username: 'Ulbi tv',
-                    avatar: 'https://xakep.ru/wp-content/uploads/2018/05/171485/KuroiSH-hacker.jpg',
-                },
-                type: [
-                    ArticleType.IT,
-                ],
-            },
-        },
-
-    },
-})];
+Normal.decorators = [StoreDecorator({})];
