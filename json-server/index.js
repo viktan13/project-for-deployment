b7699b559a5128fa74f759b2c0747aa54ff47dd2
Viktor Dzyuba <vik.tan@hotmaile.com>
2023-05-06 19:42:36 -0500

    fixed proxy query params

--- json-server/index.js
+++ json-server/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
 const https = require('https');
+const http = require('http');
 
 const options = {
     key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
@@ -60,10 +61,18 @@ server.use((req, res, next) => {
 server.use(router);
 
 const httpsServer = https.createServer(options, server);
+const httpServer = http.createServer(server);
+
+const HTTP_PORT = 8000;
 const PORT = 8443;
-// запуск сервера
+// запуск prod сервера
 httpsServer.listen(PORT, () => {
     console.log(`server is running on ${PORT} port`);
+});
+
+// запуск dev сервера
+httpServer.listen(HTTP_PORT, () => {
+    console.log(`server is running on ${HTTP_PORT} port`);
 });
 
 // for versel add  apiUrl=https://production-project-server-silk.vercel.app
